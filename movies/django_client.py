2f76ae4e3ed38d1df461eafd789ceb9687d0e730
oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2021-12-21 10:57:39 +0100

    Delete movies directory

--- movies/django_client.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-from movies.client import OmdbClient
-
-
-def get_client_from_settings():
-    """Create an instance of an OmdbClient using the OMDB_KEY from the Django settings."""
-    return OmdbClient(settings.OMDB_KEY)
